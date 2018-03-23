a3495adc46a5a7143d5b74412a75ae46078729ca
TheNech <savchenkov.vk@yandex.ru>
2018-03-23 12:24:52 +0300

    some fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app     = express();
-const port = (process.env.port || '80');
+const port = (process.env.PORT || '80');
 const https = require('https');
 
 let currencyData = '';
