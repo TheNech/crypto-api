60fc46e23a60bafd56a521d9895e9e08263127a4
TheNech <savchenkov.vk@yandex.ru>
2018-03-24 20:57:24 +0300

    add new api methods

--- server.js
+++ server.js
@@ -1,17 +1,53 @@
-const express = require('express');
-const app     = express();
-const port = (process.env.PORT || '80');
-const https = require('https');
+const express    = require('express');
+const https      = require('https');
+const bodyParser = require('body-parser');
+const mysql      = require('mysql');
+const app        = express();
+const port       = (process.env.PORT || '8000');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let currencyData = '';
+
+//mysql
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'crypto'
+});
+//-----
 
 app.listen(port, () => {
     console.log('Server started on ' + port)
 
-    let currencyUpd = setInterval(getCurrencyData, 100);
+    let currencyUpd = setInterval(getCurrencyData, 50);
 });
 app.get('/api/currency', (req, res) => {
     res.json(currencyData);
+});
+app.post('/api/tokens', (req,res) => {
+    connection.connect(function(err) {
+        if(err) throw err;
+
+        let sql = 'INSERT INTO tokens SET ?';
+        connection.query(sql, req.body, function (err, rows, fields) {
+            if(err) throw err;
+        });
+    });
+
+    res.status(200).json({});
+});
+app.post('/api/notification', (req, res) => {
+    connection.connect(function(err) {
+        if(err) throw err;
+
+        let sql = 'INSERT INTO notify SET ?';
+        connection.query(sql, req.body, function (err, rows, fields) {
+            if(err) throw err;
+        });
+    });
+
+    res.status(200).json({});
 });
 
 function getCurrencyData() {
