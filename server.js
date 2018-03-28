3dd7683bf4f3ad314160c3ae1d6772bf68c35d3c
TheNech <savchenkov.vk@yandex.ru>
2018-03-28 22:21:09 +0300

    notifications for rise and fall price

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //firebase
 let serviceAccount = require('./serviceAccount.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://test-e8d1b.firebaseio.com"
@@ -32,15 +31,15 @@ const connection = mysql.createConnection({
 app.listen(port, () => {
     logger.info('Server started on ' + port)
 
-    let currencyUpd = setInterval(getCurrencyData, 100);
-    // connection.connect(function(err) {
-    //     if(err) throw err;
-    // });   
+    let currencyUpd = setInterval(getCurrencyData, 100);  
     
+    chekNotify();
 });
+
 app.get('/api/currency', (req, res) => {
     res.json(currencyData);
 });
+
 app.post('/api/tokens', (req,res) => {
     let sqlSel = 'SELECT * FROM tokens WHERE ?'
     connection.query(sqlSel, req.body, function (err, rows, fields) {
@@ -53,19 +52,54 @@ app.post('/api/tokens', (req,res) => {
             });
         } else res.status(200).json({});
     });
-
 });
-app.post('/api/notification', (req, res) => {
-    let sql = 'INSERT INTO notify SET ?';
+
+app.post('/api/notify/max', (req, res) => {
+    let sql = 'INSERT INTO notify_max SET ?';
     connection.query(sql, req.body, function (err, rows, fields) {
         if(err) throw err;
     });
 
     res.status(200).json({});
 });
+
+app.post('/api/notify/min', (req, res) => {
+    let sql = 'INSERT INTO notify_min SET ?';
+    connection.query(sql, req.body, function (err, rows, fields) {
+        if(err) throw err;
+    });
+
+    res.status(200).json({});
+});
+
+app.post('/api/notify/max/del', (req, res) => {
+    let sql = 'DELETE FROM notify_ma xWHERE registration_id = ? AND currency = ?';
+    let arr = [];
+    arr.push(req.body.registration_id.toString());
+    arr.push(req.body.currency.toString());
+
+    connection.query(sql, arr, function (err, rows, fields) {
+        if(err) throw err;
+    });
+
+    res.status(200).json({});
+});
+
+app.post('/api/notify/min/del', (req, res) => {
+    let sql = 'DELETE FROM notify_min WHERE registration_id = ? AND currency = ?';
+    let arr = [];
+    arr.push(req.body.registration_id.toString());
+    arr.push(req.body.currency.toString());
+
+    connection.query(sql, arr, function (err, rows, fields) {
+        if(err) throw err;
+    });
+
+    res.status(200).json({});
+});
+
 app.get('/api/notification/send', (req, res) => {
-    logger.info(req.query);
-    var payload = {
+    let payload = {
       notification: {
         title: req.query.title,
         body: req.query.body
@@ -114,26 +148,48 @@ function getCurrencyData() {
     });
 }
 
-function sendNotify() {
+function chekNotify() {
     const fsyms = 'BTC,ETH,LTC,XRP,BCH,TRX,BNB,ETC,EOS,XEM,NEO,DASH,HT,NCASH,XMR,VEN,ICX,ADA,XRB,IOT,ZEC,XLM,IOST,ABT,WAVES,OMG,ELF,LSK,QTUM,ELA,POWR,MTL,MCO,NBT,EMC2,GVT,HSR,BTG,MTN*,BTM*,AST,DGD,TNT,ADX,THETA,GNT,OCN,ZIL,SUB,SRN';
     let url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms='+ fsyms + '&tsyms=USD';
-    let delArray = [];
 
-    getNotifyList.then(arr => {
-        fetch(url)
-            .then(res => res.json())
-            .then(json => {
-                if(json[arr[0].currency].USD > arr[0].price){
-                    delArray.push(arr[0].id);
-                }
-            })
-            .catch(err => console.log(err));
-    });
-    console.log(delArray);
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            getMaxNotifyList.then(arr => {
+                arr.forEach(function(item, i, arr){
+                    if(json[item.currency].USD > item.price) {
+                        sendPriceNotify('max', item.registration_id, item.currency, json[item.currency].USD);
+                        delFromTable('notify_max', item.id);
+                    }
+                });
+            });
+
+            getMinNotifyList.then(arr => {
+                arr.forEach(function(item, i, arr){
+                    if(json[item.currency].USD < item.price) {
+                        sendPriceNotify('min', item.registration_id, item.currency, json[item.currency].USD);
+                        delFromTable('notify_min', item.id);
+                    }
+                });
+            });
+        })
+        .catch(err => console.log(err));
 }
 
-let getNotifyList = new Promise(function(resolve, reject) {
-    let sql = 'SELECT * FROM notify';
+let getMaxNotifyList = new Promise(function(resolve, reject) {
+    let sql = 'SELECT * FROM notify_max';
+    let notifyList = [];
+    connection.query(sql,  function (err, rows, fields) {
+        if(err) throw err;
+        else {
+            notifyList = rows.concat([]);
+            resolve(notifyList);
+        }
+    });
+});
+
+let getMinNotifyList = new Promise(function(resolve, reject) {
+    let sql = 'SELECT * FROM notify_min';
     let notifyList = [];
     connection.query(sql,  function (err, rows, fields) {
         if(err) throw err;
@@ -158,3 +214,39 @@ let getTokens = new Promise(function(resolve, reject) {
         }
     });
 });
+
+function sendPriceNotify(flag, token, currency, price) {
+    let payload = {};
+    if(flag == 'max') {
+        payload = {
+          notification: {
+            title: currency,
+            body: "Цена повысилась и составляет $" + price
+          }
+        };
+    }
+    if(flag == 'min') {
+        payload = {
+          notification: {
+            title: currency,
+            body: "Цена понизилась и составляет $" + price
+          }
+        };
+    }
+    
+    admin.messaging().sendToDevice(token, payload)
+      .then(function(response) {
+        logger.debug('Successfully sent message:', response);
+      })
+      .catch(function(error) {
+        logger.warn('Error sending message:', error);
+      });
+}
+
+function delFromTable(table, id) {
+    let sql = "DELETE FROM " + table + " WHERE id=" + id;
+
+    connection.query(sql, function(err, rows, fields) {
+        if(err) throw err;
+    });
+}
